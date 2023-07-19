451ebc2f7acb25eb9bc0f022877544f749111aad
Esther Wango <warmbow@gmail.com>
2023-07-19 14:39:53 +0300

    add env to git ignore

--- quza/src/environments/environment.ts
+++ quza/src/environments/environment.ts
@@ -2,8 +2,13 @@
 // `ng build` replaces `environment.ts` with `environment.prod.ts`.
 // The list of file replacements can be found in `angular.json`.
 
+const zoho_client_id = "1000.Q4YMZLD8CGOGAH2K6X0W0CZ8XC219G";
 export const environment = {
-  production: false
+  production: false,
+    client_secret: "6b845bb7d125ca62b1feb124842dc1bac494af7b2a",
+  base_url: {
+    authorization_request:"https://accounts.zoho.com/oauth/v2/auth?response_type=code&client_id=1000.Q4YMZLD8CGOGAH2K6X0W0CZ8XC219G&scope=ZohoCreator.form.CREATE&redirect_uri=https://quza.co.ke/callback&access_type=offline",
+  }
 };
 
 /*
