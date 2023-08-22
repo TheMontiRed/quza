5f5ca6f9f7cc02a600e8c26e47990980cd9cef13
Esther Wango <warmbow@gmail.com>
2023-08-22 12:01:02 +0300

    Added firebase cloud functions and firestore database

--- quza/src/app/app.module.ts
+++ quza/src/app/app.module.ts
@@ -1,16 +1,29 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HttpClientModule, HttpClientJsonpModule  } from '@angular/common/http';
+import { environment } from '../environments/environment'
+
+import { provideFirebaseApp, initializeApp } from '@angular/fire/app';
+import { getFunctions, provideFunctions } from '@angular/fire/functions';
+import { getFirestore, provideFirestore } from '@angular/fire/firestore';
+import { WrapperModule } from './wrapper/wrapper.module';
+import { WrapperComponent } from './wrapper/wrapper.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    HttpClientJsonpModule,
+    WrapperModule,
+    provideFirebaseApp(() => initializeApp(environment.firebaseConfig)),
+    provideFunctions(() => getFunctions()),
+    provideFirestore(() => getFirestore()),
   ],
   providers: [],
   bootstrap: [AppComponent]
